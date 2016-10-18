c2d4322fb8eeaeb4937485ba603523b6cb23ddf0
Pablo Gómez Abajo <gomezabajo@yahoo.com>
2016-10-18 17:20:33 +0200

    ASPLE Wodel samples

---
+++ ASPLEWodelSample1/src-gen/ASPLEWodelSample1.java
@@ -0,0 +1,286 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+
+import manager.ModelManager;
+
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.resource.Resource;
+
+import commands.*;
+import commands.selection.strategies.*;
+import commands.strategies.*;
+
+import exceptions.*;
+
+import appliedMutations.*;
+
+import mutatorenvironment.MutatorEnvironment;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.common.util.EList;
+
+public class ASPLEWodelSample1 extends manager.MutatorUtils implements manager.IMutatorExecutor {
+	
+   	//RESET COUNTER: 0
+   	//RESET COUNTER: 0
+   	//RESET COUNTER: 0
+   	//RESET COUNTER: 0
+   	//RESET COUNTER: 0
+   	//RESET COUNTER: 0
+	//RESET COUNTER: 0
+	//COUNTER: 1
+	//METHOD NAME:mutation1
+	private ArrayList<Mutator> mutation1(ArrayList<EPackage> packages, Resource model, HashMap<String, EObject> hmObjects, HashMap<String, List<EObject>> hmList) throws ReferenceNonExistingException {
+		ArrayList<Mutator> mutations = new ArrayList<Mutator>();
+	//MODIFY INFORMATION mutation1
+	RandomTypeSelection rts = new RandomTypeSelection(packages, model, "BinaryOperator");
+	List<EObject> objects = rts.getObjects();
+					   	//EXPRESSION LIST: []
+					   	//EXPRESSION LEVEL: 0
+					   	//EXPRESSION LEVEL: true
+					   	Expression exp0 = new Expression();
+					   	//INDEX EXPRESSION: 
+					   	  		exp0.first = new AttributeEvaluation();
+					   	  		//ATTRIBUTE: 
+					   	   		((AttributeEvaluation) exp0.first).name = "operator";
+					   	   		((AttributeEvaluation) exp0.first).operator = "in";
+					   	((AttributeEvaluation) exp0.first).values = new ArrayList<String>();
+					   	//
+					   	((AttributeEvaluation) exp0.first).values.add("less");
+					   	((AttributeEvaluation) exp0.first).values.add("equals");
+					   	((AttributeEvaluation) exp0.first).values.add("greater");
+					   	((AttributeEvaluation) exp0.first).values.add("distinct");
+					   	   			((AttributeEvaluation) exp0.first).type = "String";
+					   	   		exp0.operator = new ArrayList<Operator>();
+					   	   		//OPNAME: 
+					   	   		exp0.second = new ArrayList<Evaluation>();
+					   	   		//EVNAME: 
+				   		List<EObject> selectedObjects = evaluate(objects, exp0);
+	EObject object = null;
+	if (selectedObjects.size() > 0) {
+		object = selectedObjects.get(ModelManager.getRandomIndex(selectedObjects));
+	}
+	ObSelectionStrategy objectSelection = null; 
+	if (object != null) {
+		objectSelection = new SpecificObjectSelection(packages, model, object);
+	}
+			
+		HashMap<String, AttributeConfigurationStrategy> atts = new HashMap<String, AttributeConfigurationStrategy>();
+		//COUNTER:
+		//COUNTER:0
+		//NAME:operator
+	if (objectSelection != null) {
+	//NAME:operator
+	atts.put("operator", new ListStringConfigurationStrategy((String) ModelManager.getAttribute("operator", objectSelection.getObject()), "[less, equals, greater, distinct]", "operator")
+	);
+	}
+	   				
+		HashMap<String, ReferenceConfigurationStrategy> refs = new HashMap<String, ReferenceConfigurationStrategy>();
+		//COUNTER:0
+	if (objectSelection != null) {
+		ModifyInformationMutator mut = new ModifyInformationMutator(model, packages, objectSelection, atts, refs);
+		//INC COUNTER: 0
+		mut.setId("m1");
+						mutations.add(mut);
+	}
+	//END MODIFY INFORMATION mutation1
+		return mutations;	
+	}
+	//COUNTER: 1
+	//METHOD NAME:registry1
+	//REGISTRY COUNTER: 0
+	private AppMutation registry1(Mutator mut, HashMap<String, EObject> hmMutator, Resource seed) {
+		AppMutation appMut = null;
+		InformationChanged icMut = AppliedMutationsFactory.eINSTANCE.createInformationChanged();
+		icMut.setObject(mut.getObject());
+		EList<AttributeChanged> attsMut = icMut.getAttChanges();
+		Object oldAttVal = null;
+		Object newAttVal = null;
+		//ATTRIBUTE COUNTER: 
+	//
+	//
+	AttributeChanged attMut0 = null;
+	attMut0 = AppliedMutationsFactory.eINSTANCE.createAttributeChanged();
+	attMut0.setAttName("operator");
+	oldAttVal = ((ModifyInformationMutator) mut).getOldAttValue("operator");
+	newAttVal = ((ModifyInformationMutator) mut).getNewAttValue("operator");
+	if (oldAttVal != null) {
+		attMut0.setOldVal(oldAttVal.toString());
+	}
+	if (newAttVal != null) {
+		attMut0.setNewVal(newAttVal.toString());
+	}
+	attMut0.setDef(hmMutator.get("m1"));
+	attsMut.add(attMut0);
+	icMut.setDef(hmMutator.get("m1"));
+	//ATTRIBUTE COUNTER INC: 0
+		appMut = icMut;
+		return appMut;
+	}
+	public void execute(int maxAttempts, int numMutants, boolean registry) throws ReferenceNonExistingException, WrongAttributeTypeException, 
+												  MaxSmallerThanMinException, AbstractCreationException, ObjectNoTargetableException, 
+ 												  ObjectNotContainedException, MetaModelNotFoundException, ModelNotFoundException, IOException {
+ 		
+		if (maxAttempts <= 0) {
+			maxAttempts = 1;
+		}
+		
+		    String ecoreURI = "C:/eclipse/runtime-EclipseApplication/ASPLEWodelSample1/model/Asple.ecore";
+		    
+		    String modelURI = "C:/eclipse/runtime-EclipseApplication/ASPLEWodelSample1/model/";
+		    String modelsURI = "C:/eclipse/runtime-EclipseApplication/ASPLEWodelSample1/out/";
+		    String resourcesURI = "C:/eclipse/runtime-EclipseApplication/ASPLEWodelSample1/resources/";
+		    
+		    HashMap<String, String> hashmapModelFilenames = new HashMap<String, String>();
+		    File[] files = new File(modelURI).listFiles();
+		    for (int i = 0; i < files.length; i++) {
+		    	if (files[i].isFile() == true) {
+		    		String pathfile = files[i].getPath();
+		    		if (pathfile.endsWith(".model") == true) {
+		    			hashmapModelFilenames.put(pathfile, modelsURI + files[i].getName().substring(0, files[i].getName().length() - ".model".length()));
+		    		}
+		    	}
+		    }
+		    
+		    //Load MetaModel
+		    ArrayList<EPackage> packages = ModelManager.loadMetaModel(ecoreURI);
+		    
+		    //Load Model
+		    Set<String> modelFilenames = hashmapModelFilenames.keySet();
+		    for (String modelFilename : modelFilenames) {
+		    	HashSet<String> hashsetMutants = new HashSet<String>();
+		    	hashsetMutants.add(modelFilename);
+		    	Resource model = ModelManager.loadModel(packages, modelFilename);
+		    	Resource seed = ModelManager.loadModel(packages, modelFilename);
+		    
+		    	HashMap<String, EObject> hashmapEObject = new HashMap<String, EObject>();
+		    	HashMap<String, List<EObject>> hashmapList = new HashMap<String, List<EObject>>();
+		    //2
+		   	numMutants = 2;
+		   	
+		   	String mutatorecore = resourcesURI + "MutatorEnvironment.ecore";
+		   	
+		   	//Load MetaModel
+		   	ArrayList<EPackage> mutatorpackages = ModelManager.loadMetaModel(mutatorecore);
+		   	Resource mutatormodel = ModelManager.loadModel(mutatorpackages, URI.createURI("file:/C:/eclipse/runtime-EclipseApplication/ASPLEWodelSample1/out/ASPLEWodelSample1.model").toFileString());
+		   	
+		   	HashMap<String, EObject> hmMutator = getMutators(ModelManager.getObjects(mutatormodel));
+		   			
+		   	   	   	for (int i = 0; i < numMutants; i++) {
+		   	   	   		String mutFilename = hashmapModelFilenames.get(modelFilename) + "/" + "Output" + i + ".model";
+		   	   	   		boolean isRepeated = true;
+		   	   			int attempts = 0;
+		   	   			int max = 0;
+		   	   			while ((isRepeated == true) && (attempts < maxAttempts)) {
+		   			Mutations muts = AppliedMutationsFactory.eINSTANCE.createMutations();
+		   	   				attempts++;
+		   	   				//RESET COUNTER: 0
+		   	   				//RESET COUNTER: 0
+		   	   				//RESET COUNTER: 0
+		   	   				//RESET COUNTER: 0
+		   	   				//RESET COUNTER: 0
+		   	   				//RESET COUNTER: 0
+		   	   				//RESET COUNTER: 0
+		   	
+		   	   	   			   	   	//COUNTER: 1	
+		   	   	   			//COMMAND: 52
+		   	   	   			//REGISTRY COUNTER: 1
+		   	   	   			max = 1;
+		   	   	   			for (int j = 0; j < max; j++) {
+		   	   	   			//NAME:52
+		   	   	   			//METHOD NAME:mutation1
+		   	   	   			
+		   	   	   			ArrayList<Mutator> l52 = mutation1(packages, model, hashmapEObject, hashmapList);
+		   	   	   			//COUNTER: 1
+		   	   	   			//REGISTRY METHOD NAME:registry1
+		   	   	   			
+		   	   	   			if (l52 != null) {
+		   	   	   				for (Mutator mut : l52) {
+		   	   	   					if (mut != null) {
+		   	   	   						EObject mutated = (EObject) mut.mutate();
+		   	   	   						if (mutated != null) {
+		   	   	   						AppMutation appMut = registry1(mut, hmMutator, seed);
+		   	   	   						if (appMut != null) {
+		   	   	   							muts.getMuts().add(appMut);
+		   	   	   						}
+		   	   	   						}
+		   	   	   					}
+		   	   	   				}
+		   	   	   			}
+		   	   	   			}
+		   	      
+		   			// COMPLETES MUTANT
+		   	   		int valid = complete(packages, model);
+		   		       		if (valid != 1) {
+		   		       			// VERIFY THE OCL CONSTRAINTS
+		   		       			HashMap<String, ArrayList<String>> rules = new HashMap<String, ArrayList<String>>();
+		   	       		if (matchesOCL(model, rules) == true) {
+		   		   					// VERIFY IF MUTANT IS DIFFERENT
+		   	   						isRepeated = different(packages, model, hashsetMutants);
+		   	   					
+		   	   						System.out.println("isRepeated: " + isRepeated);
+		   	   						// IF MUTANT IS DIFFERENT STORES IT AND PROCEEDS
+		   	   						if (isRepeated == false) {
+		   		      			File outputFolder = new File(hashmapModelFilenames.get(modelFilename));
+		   	      						if (outputFolder.exists() != true) {
+		   	      							outputFolder.mkdir();
+		   	      						}
+		   	       						ModelManager.saveOutModel(model, mutFilename);
+		   	       			      		hashsetMutants.add(mutFilename);
+		   	       			      		if (registry == true) {
+		   	       			      			Resource mutant = ModelManager.loadModel(packages, mutFilename);
+		   							for (AppMutation mut : muts.getMuts()) {
+		   	       			      				if (mut instanceof ObjectCreated) {
+		   	       			      					List<EObject> emuts = ((ObjectCreated) mut).getObject();
+		   	       			      					if (emuts.size() > 0) {
+		   	   	       			      			EObject emutated = emuts.get(0);
+		   	   	       			      			emuts.remove(0);
+		   	   	       			      			if (ModelManager.getObject(mutant, emutated) != null) {
+		   	   	       			      				emuts.add(ModelManager.getObject(mutant, emutated));
+		   	   	       			      			}
+		   	   	       			      		}
+		   								}
+		   								if (mut instanceof ReferenceCreated) {
+		   	   								List<EReference> emuts = ((ReferenceCreated) mut).getRef();
+		   	   								if (emuts.size() > 0) {
+		   	   									EReference emutated = emuts.get(0);
+		   	   									emuts.remove(0);
+		   	   									if (ModelManager.getReference(mutant, emutated) != null) {
+		   	   										emuts.add(ModelManager.getReference(mutant, emutated));
+		   	   									}
+		   	   								}
+		   	   							}
+		   	       			      			}
+		   		       			      		File registryFolder = new File(hashmapModelFilenames.get(modelFilename) + "/registry");
+		   	    	   			      		if (registryFolder.exists() != true) {
+		   	       				      			registryFolder.mkdir();
+		   	       				      		}
+		   	       				      		String registryFilename = hashmapModelFilenames.get(modelFilename) + "/registry/" + "Output" + i + "Registry.model";
+		   	       			    	  		ModelManager.createModel(muts, registryFilename);
+		   	       			      		}
+		   					}
+		   				}
+		   			}   
+		   		    		//Reload input
+		   		    		try {
+		   				model.unload();
+		   				model.load(null); 
+		   			} catch (Exception e) {}
+		   	   			}
+		   	   		}   
+		
+		}
+	}
+	
+}
+

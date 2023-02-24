65c4c73927d461664b54a803bc5da7119344c1da
Pablo Gómez-Abajo <gomezabajo@yahoo.com>
2023-02-24 13:36:48 +0100

    automated mutation engine -- acks @Faezeh-Kh

--- wodel.dsls.wodel/src/wodel/dsls/WodelUtils.java
+++ wodel.dsls.wodel/src/wodel/dsls/WodelUtils.java
@@ -1455,9 +1455,9 @@ public class WodelUtils {
 				for (String folderName : folders.substring(folders.indexOf("/"), folders.length()).split("/")) {
 					batwriter.println("cd " + folderName);
 				}
-				batwriter.println("javac -source 1.8 -target 1.8 -classpath " + currentPath + "/*;" + eclipseHomePath + "/plugins/*;" + eclipseHomePath + "/workspace/wodel.updatesite/plugins/* " + wodelProjectName + "Standalone/" + wodelProjectName + "Standalone.java " + wodelProjectName + "/" + wodelProjectName + "StandaloneAPI.java " + wodelProjectName + "/" + wodelProjectName + "StandaloneAPILauncher.java");
+				batwriter.println("javac -source 1.8 -target 1.8 -classpath " + currentPath + "/*;" + currentPath + "/lib/*;" + eclipseHomePath + "/plugins/*;" + eclipseHomePath + "/workspace/wodel.updatesite/plugins/* " + wodelProjectName + "Standalone/" + wodelProjectName + "Standalone.java " + wodelProjectName + "/" + wodelProjectName + "StandaloneAPI.java " + wodelProjectName + "/" + wodelProjectName + "StandaloneAPILauncher.java");
 				batwriter.println("cd ..");
-				batwriter.println("java -classpath .;" + currentPath + "/*;" + eclipseHomePath + "/plugins/*;" + eclipseHomePath + "/workspace/wodel.updatesite/plugins/* mutator/" + wodelProjectName +"/" + wodelProjectName + "StandaloneAPILauncher " + inputPath + " " + outputPath);
+				batwriter.println("java -classpath .;" + currentPath + "/*" + currentPath + "/lib/*;" + eclipseHomePath + "/plugins/*;" + eclipseHomePath + "/workspace/wodel.updatesite/plugins/* mutator/" + wodelProjectName +"/" + wodelProjectName + "StandaloneAPILauncher " + inputPath + " " + outputPath);
 				batwriter.println("exit");
 				batwriter.close();
 				ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", batcompile);
@@ -2514,6 +2514,7 @@ public class WodelUtils {
 					System.out.println("Compilation completed successfully!!");
 				}
 				System.out.println("Generation of the mutants from the command line...");
+				String currentPath = new java.io.File(".").getCanonicalPath();
 				String batcompile = wodelProjectPath + "/src-gen/bat/compile.bat";
 				File batfolder = new File(wodelProjectPath + "/src-gen/bat");
 				if (!batfolder.exists()) {
@@ -2528,9 +2529,9 @@ public class WodelUtils {
 					for (String folderName : folders.substring(folders.indexOf("/"), folders.length()).split("/")) {
 						batwriter.println("cd " + folderName);
 					}
-					batwriter.println("javac -source 8 -target 8 -classpath %~dp0/*;" + eclipseHomePath + "/plugins/*;" + eclipseHomePath + "/workspace/wodel.updatesite/plugins/* " + wodelProjectName + "Standalone/" + wodelProjectName + "Standalone.java " + wodelProjectName + "/" + wodelProjectName + "StandaloneAPI.java " + wodelProjectName + "/" + wodelProjectName + "StandaloneAPILauncher.java");
+					batwriter.println("javac -source 1.8 -target 1.8 -classpath " + currentPath + "/*;" + currentPath + "/lib/*;" + eclipseHomePath + "/plugins/*;" + eclipseHomePath + "/workspace/wodel.updatesite/plugins/* " + wodelProjectName + "Standalone/" + wodelProjectName + "Standalone.java " + wodelProjectName + "/" + wodelProjectName + "StandaloneAPI.java " + wodelProjectName + "/" + wodelProjectName + "StandaloneAPILauncher.java");
 					batwriter.println("cd ..");
-					batwriter.println("java -classpath .;%~dp0/*;" + eclipseHomePath + "/plugins/*;" + eclipseHomePath + "/workspace/wodel.updatesite/plugins/* mutator/" + wodelProjectName +"/" + wodelProjectName + "StandaloneAPILauncher " + inputPath + " " + outputPath);
+					batwriter.println("java -classpath .;" + currentPath + "/*" + currentPath + "/lib/*;" + eclipseHomePath + "/plugins/*;" + eclipseHomePath + "/workspace/wodel.updatesite/plugins/* mutator/" + wodelProjectName +"/" + wodelProjectName + "StandaloneAPILauncher " + inputPath + " " + outputPath);
 					batwriter.println("exit");
 					batwriter.close();
 				} catch (UnsupportedEncodingException e) {
